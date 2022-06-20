214fec938ffb9423d880ede6c4e572d44298db44
firetimer <adult@live.cn>
2022-06-20 13:19:57 +0800

    Update 20220619 v3.4.2
    Signed-off-by: firetimer <adult@live.cn>

--- autorelease.cmd
+++ autorelease.cmd
@@ -1,4 +1,4 @@
-git commit -m "Update 20220522 v3.4.1"
-git tag -s -m "tktMach 3.4.1" v3.4.1
+git commit -s -m "Update 20220619 v3.4.2"
+git tag -s -m "tktMach 3.4.2" v3.4.2
 git push --tags
 git push
