3e650eedc3a73614261126a2114d72d7bffa40cb
firetimer <adult@live.cn>
2022-07-05 11:34:53 +0800

    Minor update

--- autorelease.cmd
+++ autorelease.cmd
@@ -1,4 +1,9 @@
-git commit -s -m "Update 20220703 v3.4.3"
-git tag -s -m "tktMach 3.4.3" v3.4.3
-git push --tags
-git push
+@SET GIT=git
+@SET ECHO=ECHO
+@SET VERSHRT=3.4.4
+@SET VERDATE=20220717
+REM DO NOT leave any sensitive data or credentials HERE!
+%GIT% commit -s -m "Update %VERDATE% v%VERSHRT%"
+%GIT% tag -s -m "tktMach %VERSHRT%" v%VERSHRT%
+%GIT% push --tags
+%GIT% push
