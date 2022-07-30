27dddcaa3fdd368ecdc063efa4a1fd5faf199754
firetimer <adult@live.cn>
2022-07-30 22:51:47 +0800

    Update 20220731 v3.4.5
    Signed-off-by: firetimer <adult@live.cn>

--- autorelease.cmd
+++ autorelease.cmd
@@ -1,7 +1,7 @@
 @SET GIT=git
 @SET ECHO=ECHO
-@SET VERSHRT=3.4.4
-@SET VERDATE=20220717
+@SET VERSHRT=3.4.5
+@SET VERDATE=20220731
 REM DO NOT leave any sensitive data or credentials HERE!
 %GIT% commit -s -m "Update %VERDATE% v%VERSHRT%"
 %GIT% tag -s -m "tktMach %VERSHRT%" v%VERSHRT%
