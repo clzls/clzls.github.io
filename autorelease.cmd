522b65b4c0d1adb56afef5e13c04ca20f4f9037d
firetimer <adult@live.cn>
2022-10-03 23:27:09 +0800

    Update 20221003 v3.5.3

--- autorelease.cmd
+++ autorelease.cmd
@@ -1,14 +1,24 @@
-@SET GIT=git
-@SET ECHO=ECHO
-@SET VERSHRT=3.5.2
-@SET VERDATE=20220910
+@ECHO OFF
+SET GIT=git
+SET ECHO=ECHO
+SET VERSHRT=3.5.3
+SET VERDATE=20221003
 REM DO NOT leave any sensitive data or credentials HERE!
+
+%ECHO% %GIT% commit -S -m "Update %VERDATE% v%VERSHRT%"
 %GIT% commit -S -m "Update %VERDATE% v%VERSHRT%"
 IF ERRORLEVEL 1 GOTO EOF
+
+%ECHO% %GIT% tag -s -m "tktMach %VERSHRT%" v%VERSHRT%
 %GIT% tag -s -m "tktMach %VERSHRT%" v%VERSHRT%
 IF ERRORLEVEL 1 GOTO EOF
+
+%ECHO% %GIT% push --tags
 %GIT% push --tags
 IF ERRORLEVEL 1 GOTO EOF
+
+%ECHO% %GIT% push
 %GIT% push
 IF ERRORLEVEL 1 GOTO EOF
+
 :EOF
