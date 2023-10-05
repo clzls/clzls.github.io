dd089b3f96e3db49bcc0dca92ed1a9e1baa7a59d
clzls <16474386+clzls@users.noreply.github.com>
2023-10-06 01:48:24 +0800

    Update 20231005 v3.8.7

--- autorelease.cmd
+++ autorelease.cmd
@@ -1,8 +1,8 @@
 @ECHO OFF
 SET GIT=git
 SET ECHO=ECHO
-SET VERSHRT=3.8.0
-SET VERDATE=20230625
+SET VERSHRT=3.8.7
+SET VERDATE=20231005
 REM DO NOT leave any sensitive data or credentials HERE!
 
 %ECHO% %GIT% commit -S -m "Update %VERDATE% v%VERSHRT%"
